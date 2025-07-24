e3b40e36777fe617dfa2ff9f2d889cc290fdf820
Almuatsam <Almuatsamalfarsi@gmail.com>
2025-07-24 19:48:03 +0400

    Implemented SleepIn.java

---
+++ sleepIn.java
@@ -0,0 +1,10 @@
+public class sleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (weekday == false || vacation == true) {
+            return true;
+        }
+
+        return false;
+
+    }
+}
---
+++ sumDouble.java
@@ -0,0 +1,12 @@
+public class sumDouble {
+    //Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+    public int sumDouble(int a, int b) {
+        int sum = a + b;
+        if (a == b) {
+            sum = sum * 2;
+        }
+
+        return sum;
+    }
+//Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+}
